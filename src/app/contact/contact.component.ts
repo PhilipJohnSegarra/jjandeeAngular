e24efb5d55957dc87cf6c3a5335b3a13fea63c2d
philip <philipjohn.segarra@gmail.com>
2024-07-15 14:04:28 +0800

    contact page fix

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import emailjs from 'emailjs-com';
 
 @Component({
   selector: 'app-contact',
@@ -7,6 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  ngOnInit(): void {
+    emailjs.init('2xP70RUSkSVt7cCYZ');
+  }
 
+  sendEmail(e: Event) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_pv15i2i', 'template_vwnlre4', e.target as HTMLFormElement)
+      .then((result) => {
+        console.log(result.text);
+        alert('Email sent successfully!');
+      }, (error) => {
+        console.log(error.text);
+        alert('Failed to send email. Please try again.');
+      });
+  }
 }
